584e47cacde47210b06d290fb471459bebbf0a58
mobpolice <125884492+mobpolice@users.noreply.github.com>
2023-07-29 22:52:51 +0900

    [level 0] Title: 양꼬치, Time: 0.02 ms, Memory: 76.2 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120830. 양꼬치/양꼬치.java
@@ -0,0 +1,10 @@
+class Solution {
+    public int solution(int n, int k) {
+        int answer = 0;
+        if(n>=10){
+            k = k - (n/10);
+        }
+        answer = (12000*n)+(2000*k);
+        return answer;
+    }
+}
