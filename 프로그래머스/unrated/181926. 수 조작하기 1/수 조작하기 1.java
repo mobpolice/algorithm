87e4701ae0fb971e8b2f110f3f46e9e4b1da60fe
mobpolice <125884492+mobpolice@users.noreply.github.com>
2023-07-30 22:04:51 +0900

    [unrated] Title: 수 조작하기 1, Time: 1.16 ms, Memory: 71.3 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181926. 수 조작하기 1/수 조작하기 1.java
@@ -0,0 +1,23 @@
+class Solution {
+    public int solution(int n, String control) {
+        int answer = 0;
+        
+        for(char c : control.toCharArray()){
+            
+            if(c=='w'){
+                n += 1;
+            }else if(c=='s'){
+                n -= 1;
+            }else if(c=='d'){
+                n += 10;
+            }else{
+                n -= 10;
+            }
+            
+        }
+        
+        answer = n;
+        
+        return answer;
+    }
+}
