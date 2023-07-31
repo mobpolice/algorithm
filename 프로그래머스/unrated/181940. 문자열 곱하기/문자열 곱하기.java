8226a262310c495ad7da68b9e987394f4b6ee409
mobpolice <125884492+mobpolice@users.noreply.github.com>
2023-07-31 14:36:01 +0900

    [unrated] Title: 문자열 곱하기, Time: 0.16 ms, Memory: 73.2 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181940. 문자열 곱하기/문자열 곱하기.java
@@ -0,0 +1,13 @@
+import java.lang.*;
+
+class Solution {
+    public String solution(String my_string, int k) {
+        StringBuffer answer = new StringBuffer();
+        
+        for(int i = 0;i<k;i++){
+            answer.append(my_string);
+        }
+        
+        return answer.toString();
+    }
+}
