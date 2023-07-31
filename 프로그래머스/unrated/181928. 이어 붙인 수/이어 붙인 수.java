9085a1b2976d76c2461f2defa4c6f3d27b5052eb
mobpolice <125884492+mobpolice@users.noreply.github.com>
2023-07-31 14:08:58 +0900

    [unrated] Title: 이어 붙인 수, Time: 9.31 ms, Memory: 72.1 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181928. 이어 붙인 수/이어 붙인 수.java
@@ -0,0 +1,21 @@
+class Solution {
+    public int solution(int[] num_list) {
+        int answer = 0;
+        String odd = "";
+        String even = "";
+        
+        for(int n : num_list){
+            
+            if(n%2==1){
+                odd += n;
+            }else{
+                even += n;
+            }
+            
+        }
+        
+        answer = Integer.parseInt(odd) + Integer.parseInt(even);
+        
+        return answer;
+    }
+}
