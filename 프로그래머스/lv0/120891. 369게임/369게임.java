decc9d525cef30a545395ae764472fd8878914c0
mobpolice <125884492+mobpolice@users.noreply.github.com>
2023-08-10 10:39:53 +0900

    [level 0] Title: 369게임, Time: 2.37 ms, Memory: 77.1 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120891. 369게임/369게임.java
@@ -0,0 +1,18 @@
+import java.util.*;
+import java.util.stream.Stream;
+
+class Solution {
+    public int solution(int order) {
+        int answer = 0;
+        
+        int[] digits = Stream.of(String.valueOf(order).split("")).mapToInt(Integer::parseInt).toArray();
+        
+        for(int i=0;i<digits.length;i++){
+            if(digits[i]%3==0 && digits[i]!=0){
+                answer++;
+            }
+        }
+        
+        return answer;
+    }
+}
