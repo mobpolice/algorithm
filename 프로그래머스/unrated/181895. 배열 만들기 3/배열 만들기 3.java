46edc470f05ca890675b46be03449c0eefb58fa1
mobpolice <125884492+mobpolice@users.noreply.github.com>
2023-08-10 12:07:11 +0900

    [unrated] Title: 배열 만들기 3, Time: 2.85 ms, Memory: 80 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181895. 배열 만들기 3/배열 만들기 3.java
@@ -0,0 +1,16 @@
+import java.util.*;
+
+class Solution {
+    public int[] solution(int[] arr, int[][] intervals) {
+        ArrayList<Integer> answer = new ArrayList<Integer>();
+        
+        for(int i=intervals[0][0];i<=intervals[0][1];i++){
+            answer.add(arr[i]);
+        }
+        for(int i=intervals[1][0];i<=intervals[1][1];i++){
+            answer.add(arr[i]);
+        }
+        
+        return answer.stream().mapToInt(i->i).toArray();
+    }
+}
