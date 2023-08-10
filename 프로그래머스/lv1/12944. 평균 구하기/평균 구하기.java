6eaf86126ee155935ad0ab28c645f4c73078d8e3
mobpolice <125884492+mobpolice@users.noreply.github.com>
2023-08-10 12:59:46 +0900

    [level 1] Title: 평균 구하기, Time: 0.04 ms, Memory: 76.5 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/12944. 평균 구하기/평균 구하기.java
@@ -0,0 +1,13 @@
+class Solution {
+    public double solution(int[] arr) {
+        double answer = 0;
+        
+        for(int a : arr){
+            answer += a;
+        }
+        
+        answer = answer/arr.length;
+        
+        return answer;
+    }
+}
