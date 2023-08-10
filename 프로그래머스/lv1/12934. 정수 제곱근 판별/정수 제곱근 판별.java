008379a417c2803924c74d4babd1a91c2426ba56
mobpolice <125884492+mobpolice@users.noreply.github.com>
2023-08-10 15:22:02 +0900

    [level 1] Title: 정수 제곱근 판별, Time: 0.03 ms, Memory: 75 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/12934. 정수 제곱근 판별/정수 제곱근 판별.java
@@ -0,0 +1,14 @@
+class Solution {
+    public long solution(long n) {
+        long answer = -1;
+        
+        for(int i=1;i<=n;i++){
+            if(n/i==i && n%i==0){
+                answer = (long)Math.pow(i+1,2);
+                break;
+            }
+        }
+        
+        return answer;
+    }
+}
