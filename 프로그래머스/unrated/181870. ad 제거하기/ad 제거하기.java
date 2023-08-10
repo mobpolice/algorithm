ed8dd604c0f4c155d963640e96aa7b388de1762b
mobpolice <125884492+mobpolice@users.noreply.github.com>
2023-08-10 12:24:43 +0900

    [unrated] Title: ad 제거하기, Time: 0.97 ms, Memory: 78.7 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181870. ad 제거하기/ad 제거하기.java
@@ -0,0 +1,15 @@
+import java.util.*;
+
+class Solution {
+    public String[] solution(String[] strArr) {
+        ArrayList<String> answer = new ArrayList<String>();
+        
+        for(String str : strArr){
+            if(!str.contains("ad")){
+                answer.add(str);
+            }
+        }
+        
+        return answer.toArray(new String[answer.size()]);
+    }
+}
