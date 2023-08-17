202c5fef4dfedfcaa766c160a7d0e10ae59dbac1
mobpolice <125884492+mobpolice@users.noreply.github.com>
2023-08-17 23:40:45 +0900

    [unrated] Title: 배열 만들기 4, Time: 8.39 ms, Memory: 81.8 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181918. 배열 만들기 4/배열 만들기 4.java
@@ -0,0 +1,20 @@
+import java.util.*;
+
+class Solution {
+    public int[] solution(int[] arr) {
+        ArrayList<Integer> stk = new ArrayList<Integer>();
+        int i = 0;
+        
+        while(i<arr.length){
+            if(stk.size()==0){
+                stk.add(arr[i++]);
+            }else if(stk.size()!=0 && stk.get(stk.size()-1)<arr[i]){
+                stk.add(arr[i++]);
+            }else if(stk.size()!=0 && stk.get(stk.size()-1)>=arr[i]){
+                stk.remove(stk.size()-1);
+            }
+        }
+        
+        return stk.stream().mapToInt(j->j).toArray();
+    }
+}
