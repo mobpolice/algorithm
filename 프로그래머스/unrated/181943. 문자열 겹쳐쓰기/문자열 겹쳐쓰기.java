af2b13fd16ba5a426daf3b7e8d8c25bd4976f844
mobpolice <125884492+mobpolice@users.noreply.github.com>
2023-08-18 17:04:11 +0900

    [unrated] Title: 문자열 겹쳐쓰기, Time: 0.03 ms, Memory: 74.5 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181943. 문자열 겹쳐쓰기/문자열 겹쳐쓰기.java
@@ -0,0 +1,11 @@
+import java.lang.*;
+
+class Solution {
+    public String solution(String my_string, String overwrite_string, int s) {
+        StringBuilder answer = new StringBuilder(my_string);
+        
+        answer.replace(s, s+overwrite_string.length(), overwrite_string);
+
+        return answer.toString();
+    }
+}
