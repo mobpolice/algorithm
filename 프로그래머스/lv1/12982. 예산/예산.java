32538dfc5425d730e21cd6f752a81bd97ad72250
mobpolice <125884492+mobpolice@users.noreply.github.com>
2023-08-22 17:43:36 +0900

    [level 1] Title: 예산, Time: 0.34 ms, Memory: 76.4 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/12982. 예산/예산.java
@@ -0,0 +1,21 @@
+import java.util.*;
+
+class Solution {
+    public int solution(int[] d, int budget) {
+        int answer = 0;
+        Arrays.sort(d);
+        
+        for(int a : d){
+            budget -= a;
+            
+            if(budget<0){
+                break;
+            }else{
+                answer++;
+            }
+            
+        }
+        
+        return answer;
+    }
+}
